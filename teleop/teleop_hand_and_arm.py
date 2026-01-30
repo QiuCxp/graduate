d202463c3c467d4a845072a1ebf029d9a04f40f8
QiuCxp <1401526854@qq.com>
2026-01-30 12:14:11 +0800

    Update teleimager camera / depth logic

--- teleop/teleop_hand_and_arm.py
+++ teleop/teleop_hand_and_arm.py
@@ -151,6 +151,8 @@ if __name__ == '__main__':
     parser.add_argument('--arm', type=str, choices=['G1_29', 'G1_23', 'H1_2', 'H1'], default='G1_29', help='Select arm controller')
     parser.add_argument('--ee', type=str, choices=['dex1', 'dex3', 'inspire_ftp', 'inspire_dfx', 'brainco'], help='Select end effector controller')
     parser.add_argument('--img-server-ip', type=str, default='192.168.123.164', help='IP address of image server, used by teleimager and televuer')
+    parser.add_argument('--loco-speed-scale', type=float, default=0.2,
+                        help='Scale for controller-based locomotion speed (translation/rotation)')
     parser.add_argument('--network-interface', type=str, default=None, help='Network interface for dds communication, e.g., eth0, wlan0. If None, use default interface.')
     # mode flags
     parser.add_argument('--motion', action = 'store_true', help = 'Enable motion control mode')
@@ -551,9 +553,10 @@ if __name__ == '__main__':
                 if tele_data.left_ctrl_thumbstick and tele_data.right_ctrl_thumbstick:
                     loco_wrapper.Damp()
                 # https://github.com/unitreerobotics/xr_teleoperate/issues/135, control, limit velocity to within 0.3
-                loco_wrapper.Move(-tele_data.left_ctrl_thumbstickValue[1] * 0.3,
-                                  -tele_data.left_ctrl_thumbstickValue[0] * 0.3,
-                                  -tele_data.right_ctrl_thumbstickValue[0]* 0.3)
+                # Use a tunable scale to slow down locomotion when using controller input.
+                loco_wrapper.Move(-tele_data.left_ctrl_thumbstickValue[1] * args.loco_speed_scale,
+                                  -tele_data.left_ctrl_thumbstickValue[0] * args.loco_speed_scale,
+                                  -tele_data.right_ctrl_thumbstickValue[0] * args.loco_speed_scale)
 
             # get current robot state data.
             current_lr_arm_q  = arm_ctrl.get_current_dual_arm_q()
@@ -593,9 +596,9 @@ if __name__ == '__main__':
                         left_hand_action = [dual_gripper_action_array[0]]
                         right_hand_action = [dual_gripper_action_array[1]]
                         current_body_state = arm_ctrl.get_current_motor_q().tolist()
-                        current_body_action = [-tele_data.left_ctrl_thumbstickValue[1]  * 0.3,
-                                               -tele_data.left_ctrl_thumbstickValue[0]  * 0.3,
-                                               -tele_data.right_ctrl_thumbstickValue[0] * 0.3]
+                        current_body_action = [-tele_data.left_ctrl_thumbstickValue[1]  * args.loco_speed_scale,
+                                               -tele_data.left_ctrl_thumbstickValue[0]  * args.loco_speed_scale,
+                                               -tele_data.right_ctrl_thumbstickValue[0] * args.loco_speed_scale]
                 elif (args.ee == "inspire_dfx" or args.ee == "inspire_ftp" or args.ee == "brainco") and args.input_mode == "hand":
                     with dual_hand_data_lock:
                         left_ee_state = dual_hand_state_array[:6]
